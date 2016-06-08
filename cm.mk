86e43ae56ca58b4ee5adbd81cc49668979fea5af
limjh16 <limjh16@gmail.com>
2016-06-08 12:21:19 +0800

    Temasek Changes
    Change-Id: I2de9abc3b587b8e0fd85462d305f5b07d1be5edf

--- cm.mk
+++ cm.mk
@@ -23,3 +23,6 @@ PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m3xx TARGET_DEVICE=m3 BUILD_FINGERPRINT="samsung/m3xx/m3:4.1.2/JZO54K/I9305XXBMA6:user/release-keys" PRIVATE_BUILD_DESC="m3xx-user 4.1.2 JZO54K I9305XXBMA6 release-keys"
+
+# Custom unofficial build tag
+TARGET_UNOFFICIAL_BUILD_ID := temasek
